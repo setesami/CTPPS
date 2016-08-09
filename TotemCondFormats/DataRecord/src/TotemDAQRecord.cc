4d49cec95542c7df164af8fc63816616ba6e8538
ziaber <marcinziaber8@gmail.com>
2016-08-09 16:47:49 +0200

    removing unused files

--- TotemCondFormats/DataRecord/src/TotemDAQRecord.cc
+++
@@ -1,4 +0,0 @@
-#include "TotemCondFormats/DataRecord/interface/TotemDAQRecord.h"
-#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
- 
-EVENTSETUP_RECORD_REG(TotemDAQRecord);
