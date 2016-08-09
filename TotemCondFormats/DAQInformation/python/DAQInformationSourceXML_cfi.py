b2a80b1c088439df43026b6d48e1a492a7f57a40
ziaber <marcinziaber8@gmail.com>
2016-08-09 16:08:14 +0200

    TotemCondFormats

--- TotemCondFormats/DAQInformation/python/DAQInformationSourceXML_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-DAQInformationSourceXML = cms.ESSource("DAQInformationSourceXML",
-    verbosity = cms.untracked.uint32(0),
-    xmlFileName = cms.string('')
-)
