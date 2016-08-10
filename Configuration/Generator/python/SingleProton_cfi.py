1096d6ea825d16cc8b154fd300aa814492c46a2b
ziaber <marcinziaber8@gmail.com>
2016-08-10 14:27:54 +0200

    changed proton gun config

--- Configuration/Generator/python/SingleProton_cfi.py
+++ Configuration/Generator/python/SingleProton_cfi.py
@@ -3,13 +3,13 @@ import FWCore.ParameterSet.Config as cms
 generator = cms.EDProducer("FlatRandomEGunProducer",
     PGunParameters = cms.PSet(
         PartID = cms.vint32(2212),
-        MaxEta = cms.double(8),
+        MaxEta = cms.double(12),
         MaxPhi = cms.double(3.141592654),
-        MinEta = cms.double(-8),
-        MinE = cms.double(64999),
+        MinEta = cms.double(8),
+        MinE = cms.double(6499),
         MinPhi = cms.double(-3.141592654), ## in radians
 
-        MaxE = cms.double(65000)
+        MaxE = cms.double(6500)
     ),
     Verbosity = cms.untracked.int32(10), ## set to 1 (or greater)  for printouts
 
